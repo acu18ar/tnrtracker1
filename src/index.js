f46454a721e57e4451ed26fddfed601211dca045
Ariel Acuna <acu18ar@gmail.com>
2020-05-21 01:43:00 -0400

    se logro vistaspor lo menos la principal

--- src/index.js
+++ src/index.js
@@ -17,7 +17,8 @@ app.engine('.hbs', exphbs({
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
-    exname: '.hbs',
+    sidebarDir: path.join(app.get('views'),  'sidebar'),
+    extname: '.hbs',
     helpers: require('./lib/handlebars')
 
 }));
--- src/routes/links.js
+++ src/routes/links.js
@@ -4,4 +4,32 @@ const router = express.Router();
 //  (../) es para subir dos niveles e ir a database
 const pool =require('../database');
 
+// para las rutas de las vistas para pedir al servidor
+
+// locahost:4000/links....
+router.get('/add', (req, res) => {
+    //res.send('Form');
+    //res.render('/links/add');
+    res.render('links/add');
+
+});
+
+router.post('/add', (req, res) => {
+    res.send('RECIBIDO');
+
+});
+
+router.get('/tracker1', (req, res) => {
+    //res.send('Form');
+    //res.render('/links/add');
+    res.render('links/tracker1');
+
+});
+router.post('/tracker1', (req, res) => {
+    res.send('No Molestes');
+
+});
+
+
+
 module.exports = router;
